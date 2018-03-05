94b51ed34975217219061d75b45d024e7f7848ee
Jakub Janas <jakub.janas08@gmail.com>
2018-03-05 11:04:02 +0000

    Add files

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Slider slider;
+
+    private float healthPoints;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        healthPoints = 100;
+        slider.value = healthPoints;
+	}
+
+    public float GetHealthPoints()
+    {
+        return healthPoints;
+    }
+
+    public void ChangeHealthPoints(float modifier)
+    {
+        healthPoints += modifier;
+        slider.value = healthPoints;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed; // a variable that will control the speed of the character
+
+    private Rigidbody rb;
+
+    private bool isMoving; // a variable that will track if the character is moving
+    private Vector3 destination; // the destination of the character provided by the 
+
+	private void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        isMoving = false;
+	}
+	
+	private void Update ()
+    {
+        KeyboardMovement();
+        MouseMovement();
+
+        if (isMoving)
+        {
+            Movement();
+        }
+
+        LimitPosition();
+    }
+
+    private void KeyboardMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = rb.transform.position + new Vector3(horizontal, 0.0f, vertical); // calculate player's movement
+        if (!movement.Equals(rb.transform.position))
+        {
+            isMoving = false;
+            rb.position = movement * speed; // assign the new position with the speed
+        }
+        
+    }
+
+    private void MouseMovement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 moveTo = hit.point;
+
+                destination = moveTo;
+            }
+        }
+    }
+
+    private void Movement()
+    {
+        if (rb.transform.position.Equals(destination))
+        {
+            isMoving = false;
+        }
+        else
+        {
+            rb.transform.position = Vector3.MoveTowards(rb.transform.position, destination, speed/2);
+        }
+    }
+
+    private void LimitPosition()
+    {
+        Vector3 currentPosition = rb.transform.position;
+
+        currentPosition.x = Mathf.Clamp(currentPosition.x, -220.0f, 220.0f); // clamp the position of the player
+        currentPosition.z = Mathf.Clamp(currentPosition.z, -220.0f, 220.0f); // so it doesn't go out of the screen
+        currentPosition.y = 1.0f; // ensure that the character is at the appropriate height
+
+        rb.transform.position = currentPosition;
+    }
+}
