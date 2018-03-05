38bdcdbf3b82df8657fae6c5441a8fb59d9f060a
Jakub Janas <jakub.janas08@gmail.com>
2018-03-05 11:47:24 +0000

    Add Player's attack
    Added a script that allows the player to attack.

--- MajorProject/Assets/Scripts/PlayerAttack.cs
+++ MajorProject/Assets/Scripts/PlayerAttack.cs
@@ -4,14 +4,52 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Triggered by enemy!");
+    private bool isInRange;
+    private List<GameObject> enemiesInRange;
 
+    private void Start()
+    {
+        isInRange = false;
+        enemiesInRange = new List<GameObject>();
+    }
+
+    private void Update()
+    {
         if (Input.GetMouseButtonDown(1))
         {
-            float damage = Mathf.Round(Random.Range(10, 25));
-            other.GetComponent<EnemyHealth>().ChangeHealthPoints(damage);
+            Debug.Log("Attacking!");
+            if (isInRange)
+            {
+                float damage = Mathf.Round(Random.Range(10, 25));
+
+                foreach(GameObject enemy in enemiesInRange)
+                {
+                    enemy.GetComponent<EnemyHealth>().ChangeHealthPoints(-damage);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Triggered by enemy!");
+            isInRange = true;
+            enemiesInRange.Add(other.gameObject);
+        }        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (enemiesInRange.Contains(other.gameObject))
+        {
+            enemiesInRange.Remove(other.gameObject);
+        }
+
+        if (enemiesInRange.Count == 0)
+        {
+            isInRange = false;
         }
     }
 }
