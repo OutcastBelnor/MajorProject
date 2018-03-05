ead9140a2dc85a63570032b22f63490a33a92c62
Jakub Janas <jakub.janas08@gmail.com>
2018-03-05 10:59:02 +0000

    Add files

---
+++ Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private float enemyHealthPoints;
+
+    private void Start()
+    {
+        enemyHealthPoints = 50;
+    }
+
+    public void ChangeHealthPoints(float modifier)
+    {
+        enemyHealthPoints += modifier;
+        Debug.Log("Enemy health: " + enemyHealthPoints);
+
+        if (enemyHealthPoints <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
