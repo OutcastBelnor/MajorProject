678c0bb21249ff4b04e5b61b5edaba7f054932aa
Jakub Janas <jakub.janas08@gmail.com>
2018-04-16 15:15:49 +0100

    Create GroupManager
    Created a Group which will keep track of the group and appoint Leaders. Created a Flocking script for Enemies

---
+++ MajorProject/Assets/Flocking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flocking : MonoBehaviour
+{
+    private GameObject leader;
+    private Vector3 destination;
+
+	void Start ()
+    {
+        leader = gameObject.GetComponentInParent<GroupManager>().GetLeader();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MajorProject/Assets/Scripts/Group/GroupManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for the group of enemies.
+/// When spawned it will appoint a leader of the group and make other enemies followers.
+/// </summary>
+public class GroupManager : MonoBehaviour
+{
+    private List<GameObject> members;
+    public GameObject leader;
+
+	void Start ()
+    {
+        AcquireMembers();
+
+        AppointLeader();
+    }
+
+    /// <summary>
+    /// Gets current leader of the group.
+    /// </summary>
+    /// <returns>leader</returns>
+    public GameObject GetLeader()
+    {
+        return leader;
+    }
+
+    /// <summary>
+    /// Adds all the members of the group to the list members.
+    /// </summary>
+    private void AcquireMembers()
+    {
+        members = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            members.Add(child.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// This method generates a random index that chooses the leader from members list.
+    /// </summary>
+    private void AppointLeader()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, members.Count);
+
+        leader = members[randomIndex];
+    }
+    
+    void Update ()
+    {
+        CheckGroup();
+
+        CheckLeader();
+	}
+
+    /// <summary>
+    /// Checks if there are any members in this group.
+    /// If no, then destroy the group.
+    /// </summary>
+    private void CheckGroup()
+    {
+        if (members.Count.Equals(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Checks if the leader is dead.
+    /// If yes, then appoint new leader.
+    /// </summary>
+    private void CheckLeader()
+    {
+        if (!leader.activeSelf)
+        {
+            AppointLeader();
+        }
+    }
+}
