98ccc0b85072f64a292c15450dadf5f02f6e1273
Jakub Janas <jakub.janas08@gmail.com>
2018-04-23 21:26:26 +0100

    Add PlayerIntensity
    Added PlayerIntensity script that handles the intensity measure which will be used by AI Director to spawn enemies

---
+++ MajorProject/Assets/Scripts/AI Director/PlayerIntensity.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIntensity : MonoBehaviour
+{
+    private int intensity = 0;
+    private bool isInCombat;
+
+    private int maxIntensity = 100;
+
+    private void Update()
+    {
+        if (!isInCombat)
+        {
+            InvokeRepeating("Decrease", 0.0f, 1.0f);
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
+    /// <summary>
+    /// Decreases the intensity, when not in combat.
+    /// </summary>
+    private void Decrease()
+    {
+        if (intensity > 0)
+        {
+            intensity--;
+        }
+    }
+
+    /// <summary>
+    /// Increases the intensity.
+    /// </summary>
+    /// <param name="amount"></param>
+    public void Increase(int amount)
+    {
+        intensity += amount;
+
+        if (intensity >= maxIntensity) // Checks if it doesn't go over the max
+        {
+            intensity = maxIntensity;
+        }
+    }
+}
